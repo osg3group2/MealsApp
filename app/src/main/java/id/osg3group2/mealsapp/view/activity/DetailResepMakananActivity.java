7530a56ea5452e51e7305c06b818d1d0f42a918b
muasmakkode <muasstepa7@gmail.com>
2019-03-23 18:13:13 +0700

    issue-6 -> xml detailnya

---
+++ app/src/main/java/id/osg3group2/mealsapp/view/activity/DetailResepMakananActivity.java
@@ -0,0 +1,19 @@
+package id.osg3group2.mealsapp.view.activity;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.osg3group2.mealsapp.R;
+
+public class DetailResepMakananActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_resep_makanan);
+
+
+    }
+}
